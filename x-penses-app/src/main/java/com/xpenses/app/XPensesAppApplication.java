a463265e050a7ebd43baacfb40eb1241a1f2e4e0
Bruno Queiroz <bruno.queiroz@liferay.com>
2024-05-23 20:25:14 -0300

    Create and configuring database setupt with docker-compose.yml

---
+++ x-penses-app/src/main/java/com/xpenses/app/XPensesAppApplication.java
@@ -0,0 +1,13 @@
+package com.xpenses.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XPensesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XPensesAppApplication.class, args);
+    }
+
+}
